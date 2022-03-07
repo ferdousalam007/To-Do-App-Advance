6cfd669717e4c8c3523e254f39e68b436e1c5f06
ferdousalam007 <iamferdousalam007@gmail.com>
2022-03-07 13:18:45 +0600

    delete task and check task

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-window.onload = function () {
+window.onload = load;
+function load() {
+    tbody.innerHTML = '';
     const tasks = getDataLocalStorage();
     tasks.forEach((task, i) => {
         showUi(task, i + 1)
     });
-    
+
 }
 //get all id
 function getId(id) {
@@ -13,9 +15,9 @@ function getId(id) {
 const form = getId('form');
 const date = getId('date');
 const tbody = getId('tableBody');
-const deleteBtn =getId('delete');
-const checkBtn =getId('check');
-const updateBtn =getId('update');
+const deleteBtn = getId('delete');
+const checkBtn = getId('check');
+const updateBtn = getId('update');
 let todaytDate = new Date().toISOString().slice(0, 10);
 date.value = todaytDate;
 
@@ -56,20 +58,21 @@ form.addEventListener('submit', function (e) {
 //get task show on UI
 function showUi({ name, priority, status, date, id }, index) {
     const tr = document.createElement('tr');
-    tr.innerHTML = `
+    tr.innerHTML =`
                     <tr>
-                        <td>${index}</td>
-                        <td>${name}</td>
-                        <td>${priority}</td>
-                        <td>${status}</td>
-                        <td>${date}</td>
-                        <td class="td_button">
+                        <td id ='no'>${index}</td>
+                        <td id ='name'>${name}</td>
+                        <td id ='priority'>${priority}</td>
+                        <td id ='status' data-bg=${status}>${status}</td>
+                        <td id ='date'>${date}</td>
+                        <td id ='action' class="td_button">
                         <button id='update' class="mr"><i class="fas fa-pen-fancy"></i></button>
                         <button id='check' class="mr"><i class="fas fa-check"></i></button>
                         <button id='delete'><i class="fas fa-trash"></i></button>
                         </td>
                     </tr>
 `;
+
     tr.dataset.id = id;
     tbody.appendChild(tr);
 }
@@ -91,37 +94,67 @@ function setDataLocalStorage(tasks) {
     localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 //---------------------action-------------------------//
-tbody.addEventListener('click',function(e){
-   
-    if(e.target.id==='delete'){
-        console.log('hh');
+tbody.addEventListener('click', function (e) {
+
+    if (e.target.id === 'delete') {
+        deleteTask(e)
     }
-    else if(e.target.id==='check'){
-        console.log('hh2');
+    else if (e.target.id === 'check') {
+        check(e)
     }
-   else if(e.target.id==='update'){
+    else if (e.target.id === 'update') {
         console.log('hh3');
     }
 })
 
-// function deleteTusk() {
-//     const getTask = getDataLocalStorage();
-    
-//     tbody.addEventListener('click', function (e) {
-//         const targetElement = e.target.parentElement.parentElement.dataset.id;
-//         console.log(targetElement);
-//         let index;
-//         getTask.forEach((task,i) => {
-//             if (task.id===targetElement) {
-//                index=i;
-//                console.log(index);
-                
-//             }
-//         })
+//01-action---- delete action
+function deleteTask(e) {
+    const tr = e.target.parentElement.parentElement;
+    const id = tr.dataset.id
+    tr.remove();
+    const tasks = getDataLocalStorage();
+    let deleteId;
+    const newID = tasks.filter(task => {
+        if (task.id == id) {
+            return task;
+        }
+    })
+    tasks.splice(newID, 1);
+    setDataLocalStorage(tasks);
 
-//         getTask.splice(index,1);
-       
-//         // setDataLocalStorage(getTask);
+    load();
 
-//     })
-// }
+}
+//02-action---- status check-button action
+function check(e) {
+    const tr = e.target.parentElement.parentElement;
+    const id = tr.dataset.id;
+    const allTd = tr.children;
+    let tasks = getDataLocalStorage();
+    [...allTd].forEach(td => {
+        
+        if (td.id == 'status') {
+            
+            tasks.forEach(task => {
+                if (task.id === id) {
+                    if (task.status === 'incomplete') {
+                        task.status = 'complete';
+                        td.innerHTML = 'complete';
+                        
+
+                        // return task;
+                    } else {
+                        task.status = 'incomplete';
+                        td.innerHTML = 'incomplete';
+
+                        // return task;
+                    }
+                };
+            })
+
+        }
+    })
+  
+    setDataLocalStorage(tasks);
+    load();
+}
